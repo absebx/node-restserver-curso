b3174b4ca710dcc80c124b0c1f55aca1ced59560
asder707 <abn.sebick@gmail.com>
2018-09-26 14:12:11 -0300

    rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //==================================
 //si no existe, se le asigna de desarrollo
 process.env.PORT = process.env.PORT || 3000;
+
+//==================================
+//Entorno
+//==================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev' //si no existe, estoy en desarrollo
+
+//==================================
+//Entorno
+//==================================
+let urlDB;
+if (process.env.NODE_ENV === 'dev'){
+  urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+  urlDB = 'mongodb://cafe-user:chocolate123456@ds051523.mlab.com:51523/cafe';
+}
+
+process.env.URLDB = urlDB;
+
+
+
+
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,7 +5,35 @@ const Usuario = require('../models/usuario');
 const app = express();
 
 app.get('/usuario', function (req, res) {
-  res.json('get usuario local')
+
+  let desde = req.query.desde || 0; //cuando son opcionales
+  desde = Number(desde);
+
+  let limite = req.query.limite || 5;
+  limite = Number(limite);
+
+  let condicion = {estado: true}
+
+  Usuario.find(condicion, 'nombre email role estado google img') //los campos que se mandarán
+    .skip(desde)
+    .limit(limite)
+    .exec((err,usuarios)=>{
+      if(err){
+        return res.status(400).json({
+          ok: false,
+          err
+        });
+      }
+
+      Usuario.countDocuments(condicion,(err,conteo)=>{
+        res.json({
+          ok: true,
+          total: conteo,
+          usuarios
+        })
+      })
+      
+    });
 });
 
 app.post('/usuario', function (req, res) {
@@ -61,8 +89,57 @@ app.put('/usuario/:id', function (req, res) {
 
 });
 
-app.delete('/usuario', function (req, res) {
-  res.json('delete usuario')
+app.delete('/usuario/:id', function (req, res) {
+  let id = req.params.id;
+  let propiedades = {
+    estado: false
+  }
+  Usuario.findByIdAndUpdate(id,propiedades,{new: true},(err,usuarioDB)=>{
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+    if(usuarioDB){
+      res.json({
+        ok:true,
+        usuario: usuarioDB
+      });
+    }else{
+      res.json({
+        ok:false,
+        err:  {
+          message: 'Usuario no encontrado'
+        }
+      });
+    }
+  });
+
+  //borrar fisicamente
+  // Usuario.findByIdAndRemove(id,(err,usuarioBorrado)=>{
+  //   if(err){
+  //     return res.status(400).json({
+  //       ok: false,
+  //       err
+  //     });
+  //   }
+
+  //   if(usuarioBorrado){
+  //     res.json({
+  //       ok: true,
+  //       usuario: usuarioBorrado
+  //     });
+  //   }else{
+  //     res.json({
+  //       ok: false,
+  //       err: {
+  //         message: 'Usuario no encontrado'
+  //       }
+  //     });
+  //   }
+  // });
+
 })
 
 module.exports = app;
